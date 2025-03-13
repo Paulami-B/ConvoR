3bc499f4865f11f4fd9ce40ac3ed864e72655c3c
Paulami-B <paulamiibanerjee@gmail.com>
2025-03-13 15:37:04 +0530

    Google OAuth added

--- client/src/components/GoogleAuth.tsx
+++ client/src/components/GoogleAuth.tsx
@@ -1,6 +1,17 @@
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { FcGoogle } from "react-icons/fc";
+import { googleOAuthURL } from "../utils/routes";
+import { toast } from "react-toastify";
+
 export default function GoogleAuth() {
+    const handleLoginWithGoogle = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+        onError: tokenResponse => console.log(tokenResponse)
+    });
     return (
-        <button className='border-2 border-orange-500 hover:border-yellow-400 px-5 py-2 rounded-full font-semibold hover:font-bold cursor-pointer'>
+        <button onClick={() => handleLoginWithGoogle()} className='flex gap-3 border-2 border-orange-500 hover:border-yellow-400 px-5 py-2 rounded-full font-semibold hover:font-bold cursor-pointer'>
+            <FcGoogle size={25} />
             Continue with Google
         </button>
     )
---
+++ client/src/utils/routes.ts
@@ -0,0 +1,1 @@
+export const googleOAuthURL = 'https://www.googleapis.com/oauth2/v3/userinfo';
